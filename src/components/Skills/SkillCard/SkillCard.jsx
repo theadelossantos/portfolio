0e927249be3d852133c940cff5398b433644d06f
Thea Clarisse P. Delos Santos <theadelossantos08@gmail.com>
2024-04-14 22:51:33 +0800

    update

--- src/components/Skills/SkillCard/SkillCard.jsx
+++ src/components/Skills/SkillCard/SkillCard.jsx
@@ -3,10 +3,10 @@ import "./SkillCard.css";
 
 const SkillCard = ({title, iconUrl, isActive, onClick}) => {
   return (
-   <div className={`skills-card ${isActive ? "active" : ""} rounded-lg border border-black p-10 text-center relative cursor-pointer hover:text-white`}
+   <div className={`skills-card ${isActive ? "active" : ""} rounded-md border border-black p-4 text-center relative cursor-pointer hover:text-white`}
    onClick={() => onClick()}>
        <div className='skill-icon'>
-            <i className="skill-icon2 material-symbols-outlined text-6xl flex items-center justify-center rounded-lg border hover:text-white text-white absolute">{iconUrl}</i>
+            <i className="skill-icon2 material-symbols-outlined text-3xl flex items-center justify-center rounded-lg border hover:text-white text-white absolute">{iconUrl}</i>
         </div> 
         <span className={`${isActive ? "text-white":""} text-lg font-medium`}>{title}</span>
    </div>
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,31 +1,52 @@
 import React, { useState } from 'react'
+import "./skills.css";
 import { SKILLS } from '../../utils/data'
 import SkillCard from './SkillCard/SkillCard'
-import "./skills.css";
 import SkillsInfo from './SkillsInfo/SkillsInfo';
-
+import { animate, motion } from 'framer-motion';
+import skillsPic from "../../assets/skills.png";
+const variants = {
+  initial:{
+    x:0,
+    y:100,
+    opacity:0
+  },
+  animate:{
+    x:0,
+    opacity: 1, 
+    y:0,
+    transition:{
+      duration: 1,
+      staggerChildren: 0.1
+    }
+  }
+}
 const Skills = () => {
   const [selectedSkill, setSelectedSkill] = useState(SKILLS[0]);
   const handleSelectSkill = (data) =>{
     setSelectedSkill(data);
   }
   return (
-    <div className="m-6 relative">
-        <h1 className="text-2xl font-semibold mb-1">Skills</h1>
+    // <motion.div className="m-6 relative" variants={variants} initial="initial" animate="animate">
+    //     <h1 className="text-2xl font-semibold mb-1">Skills</h1>
         
-        <div className="skills-content flex items-start gap-12 m-6">
-            <div className="skills flex-1 grid">
-                {SKILLS.map((item)=>(
-                    <SkillCard key={item.title} iconUrl={item.icon} title={item.title} isActive={selectedSkill.title == item.title} 
-                    onClick={()=> {handleSelectSkill(item)}}/>
-                ))}
-            </div>
-            <div className="skills-info flex-1">
-                <SkillsInfo heading={selectedSkill.title} skills={selectedSkill.skills}></SkillsInfo>
-            </div>
-        </div>
+    //     <motion.div className="skills-content items-start gap-4 m-6" variants={variants}>
+    //         <motion.div className="skills flex gap-4">
+    //             {SKILLS.map((item)=>(
+    //                 <SkillCard key={item.title} iconUrl={item.icon} title={item.title} isActive={selectedSkill.title == item.title} 
+    //                 onClick={()=> {handleSelectSkill(item)}}/>
+    //             ))}
+    //         </motion.div>
+    //         <motion.div className="skills-info flex-1" variants={variants}>
+    //             <SkillsInfo heading={selectedSkill.title} skills={selectedSkill.skills}></SkillsInfo>
+    //         </motion.div>
+    //     </motion.div>
 
-    </div>
+    // </motion.div>
+    <motion.div className="home__img flex justify-center items-center h-screen" variants={variants} initial="initial" animate="animate">
+      {/* <p className="text-6xl font-semibold mb-1 md:text-8xl ">Skilled <span className="font-light">in</span></p> */}
+          <img className="w-2/5" src={skillsPic}/>
+    </motion.div>
   )
 }
 
